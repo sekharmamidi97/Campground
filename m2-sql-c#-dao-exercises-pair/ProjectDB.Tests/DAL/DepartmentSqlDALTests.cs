f5f234b24a7b601f916d16a423bb227aeedf7cfa
Brian Cobb <brianc583@gmail.com>
2016-10-11 15:54:43 -0400

    Tests created and passed

---
+++ m2-sql-c#-dao-exercises-pair/ProjectDB.Tests/DAL/DepartmentSqlDALTests.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectDB.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Data.SqlClient;
+using ProjectDB.Models;
+
+namespace ProjectDB.DAL.Tests
+{
+    [TestClass()]
+    public class DepartmentSqlDALTests
+    {
+        private TransactionScope tran;
+        private string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Project;User ID=te_student;Password=techelevator";
+
+        private int departmentId = 0;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            tran = new TransactionScope();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd3 = new SqlCommand("delete from project_employee", conn);
+                cmd3.ExecuteNonQuery();
+
+
+                SqlCommand cmd0 = new SqlCommand("delete from employee", conn);
+                cmd0.ExecuteNonQuery();
+
+                SqlCommand cmd = new SqlCommand("delete from department", conn);
+                cmd.ExecuteNonQuery();
+
+                SqlCommand cmd2 = new SqlCommand("insert into Department (name) Values ('DummyDepartment'); SELECT CAST(SCOPE_IDENTITY() as int);", conn);
+                //cmd2.ExecuteNonQuery();
+                departmentId = (int)cmd2.ExecuteScalar();
+
+               
+            }
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            tran.Dispose();
+        }
+
+        [TestMethod()]
+        public void GetDepartmentsTest()
+        {
+            DepartmentSqlDAL dal = new DepartmentSqlDAL(connectionString);
+            List<Department> departments = dal.GetDepartments();
+
+            Assert.AreEqual(1, departments.Count);
+            Assert.AreEqual("DummyDepartment", departments[0].Name);
+        }
+
+        [TestMethod()]
+        public void CreateDepartmentTest()
+        {
+            int intialCount = GetDepartmentCount();
+            DepartmentSqlDAL dal = new DepartmentSqlDAL(connectionString);
+            Department newdept = new Department();
+            newdept.Name = "moreDummies";
+            bool result = dal.CreateDepartment(newdept);
+
+            //List<Department> departments = dal.CreateDepartment(newdept);
+
+            Assert.AreEqual(intialCount + 1, GetDepartmentCount());
+
+
+        }
+
+
+        private int GetDepartmentCount()
+        {
+            int output = 0;
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT COUNT(*) from department;", conn);
+                output = (int)cmd.ExecuteScalar();
+            }
+            return output;
+        }
+
+        [TestMethod()]
+        public void UpdateDepartmentTest()
+        {
+            DepartmentSqlDAL dal = new DepartmentSqlDAL(connectionString);
+
+            Department newdept = new Department();
+            newdept.Name = "moreCrashTestDummies";
+            newdept.Id = departmentId;
+            
+            bool result = dal.UpdateDepartment(newdept);
+
+            Assert.AreEqual(true, result);
+
+
+        }
+    }
+}
---
+++ m2-sql-c#-dao-exercises-pair/ProjectDB.Tests/DAL/ProjectSqlDALTests.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectDB.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Data.SqlClient;
+using ProjectDB.Models;
+
+namespace ProjectDB.DAL.Tests
+{
+    [TestClass()]
+    public class ProjectSqlDALTests
+    {
+        private TransactionScope tran;
+        private string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Project;User ID=te_student;Password=techelevator";
+
+        private int projectId = 0;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            tran = new TransactionScope();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("delete from project_employee", conn);
+                cmd.ExecuteNonQuery();
+
+                SqlCommand cmd1 = new SqlCommand("delete from project", conn);
+                cmd1.ExecuteNonQuery();
+
+                SqlCommand cmd2 = new SqlCommand("Insert into project (name, from_date, to_date) values ('JoshesEvilProject', '2010-12-12', '2011-12-12'); SELECT CAST(SCOPE_IDENTITY() as int);", conn);
+                projectId = (int)cmd2.ExecuteScalar();
+
+
+            }
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            tran.Dispose();
+        }
+
+        [TestMethod()]
+        public void GetAllProjectsTest()
+        {
+            ProjectSqlDAL dal = new ProjectSqlDAL(connectionString);
+            List<Project> projects = dal.GetAllProjects();
+
+            Assert.AreEqual(1, projects.Count);
+            Assert.AreEqual("JoshesEvilProject", projects[0].Name);
+
+
+        }
+
+        [TestMethod()]
+        public void AssignEmployeeToProjectTest()
+        {
+            int initialCount = GetProjectCount();
+            ProjectSqlDAL dal = new ProjectSqlDAL(connectionString);
+            dal.AssignEmployeeToProject(projectId, 1);
+
+            Assert.AreEqual(initialCount + 1, GetProjectCount());
+
+        }
+
+        [TestMethod()]
+        public void RemoveEmployeeFromProjectTest()
+        {
+            int initialCount = GetProjectCount();
+            ProjectSqlDAL dal = new ProjectSqlDAL(connectionString);
+            dal.AssignEmployeeToProject(projectId, 1);
+            Assert.AreEqual(initialCount + 1, GetProjectCount());
+            dal.RemoveEmployeeFromProject(projectId, 1);
+            Assert.AreEqual(initialCount, GetProjectCount());
+        }
+
+        [TestMethod()]
+        public void CreateProjectTest()
+        {
+
+            int intialCount = GetProjectCountAgain();
+            ProjectSqlDAL dal = new ProjectSqlDAL(connectionString);
+            Project newproj = new Project();
+            newproj.Name = "moreJoshes";
+            newproj.StartDate = new DateTime(2011, 12, 15);
+            newproj.EndDate = new DateTime(2012, 6, 15);
+
+
+
+            bool result = dal.CreateProject(newproj);
+
+            //List<Department> departments = dal.CreateDepartment(newdept);
+
+            Assert.AreEqual(intialCount + 1, GetProjectCountAgain());
+        }
+
+
+        private int GetProjectCount()
+        {
+            int output = 0;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT COUNT(*) from project_employee;", conn);
+                output = (int)cmd.ExecuteScalar();
+            }
+            return output;
+        }
+        private int GetProjectCountAgain()
+        {
+            int output = 0;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT COUNT(*) from project;", conn);
+                output = (int)cmd.ExecuteScalar();
+            }
+            return output;
+        }
+    }
+}

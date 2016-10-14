f610f45692905799c0fac325218bf045cd7bda93
Brian Cobb <brianc583@gmail.com>
2016-10-13 21:31:03 -0400

    keep wading through the murk

--- m2-csharp-capstone/Capstone/CLI/ProjectCLI.cs
+++ m2-csharp-capstone/Capstone/CLI/ProjectCLI.cs
@@ -47,6 +47,43 @@ namespace Capstone.CLI
                             GetCampgroundNow(commandNumber);
                             Console.WriteLine();
                         }
+                        else if (menuChoice == 2)
+                        {
+
+                            Console.WriteLine("1) Search For Available Reservations");
+                            Console.WriteLine("2) Return to previous screen");
+                            string choice = Console.ReadLine();
+
+                            while (choice !="1897")
+                            {
+                                if (choice == "1")
+                                {
+                                    GetCampgroundNow(commandNumber);
+                                    Console.WriteLine("Please input a Campground Id:");
+                                    string campgroundIdDtring = Console.ReadLine();
+                                    int campgroundIdInt = int.Parse(campgroundIdDtring);
+                                    Console.WriteLine();
+                                    Console.WriteLine("Please input a start date for your reservation mm/dd/yyyy:");
+                                    string startDateString = Console.ReadLine();
+                                    DateTime startDate = DateTime.Parse(startDateString);
+                                    Console.WriteLine();
+                                    Console.WriteLine("Please input an end date for your reservation mm/dd/yyyy");
+                                    string endDateString = Console.ReadLine();
+                                    DateTime endDate = DateTime.Parse(endDateString);
+                                    ReservationAvailable(campgroundIdInt, startDate, endDate);
+
+
+                                }
+                                else if(choice == "2")
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Please Try Again");
+                                }
+                            }
+                        }
                         else if (menuChoice == 3)
                         {
                             break;
@@ -99,9 +136,28 @@ namespace Capstone.CLI
                 count = count + 1;
                 Console.WriteLine($"{count}) {currentCamp.Name} | {dal.NumberToMonth(currentCamp.DateOpen)} | {dal.NumberToMonth(currentCamp.DateClosed)} | {currentCamp.DailyFee.ToString("c")}");
             });
-            
-    
 
+        }
+        private void ReservationAvailable(int campground, DateTime start, DateTime end)
+        {
+            SiteDAL dal = new SiteDAL(databaseConnection);
+            List<Reservation> reservations = dal.GetReservation(campground);
+            List<Site> sites = dal.GetSites(campground);
+            List<Reservation> booked = new List<Reservation>();
+            Console.WriteLine("Site Id | Max Occupancy | Acessible | Max RV Size | Utilities");
+            reservations.ForEach(res =>
+            {
+                bool overlap = res.StartDate < end && start < res.EndDate;
+                if (overlap)
+                {
+                    booked.Add(res);
+                }
+                else
+                {
+                    Console.WriteLine($"{res.SiteId} {sites[res.SiteId-1].MaxOccupancy} {sites[res.SiteId - 1].isAccessible} {sites[res.SiteId - 1].MaxRVLength} {sites[res.SiteId - 1].hasUtilities}"); 
+                }
+            });
+            
 
         }
     }
--- m2-csharp-capstone/Capstone/DAL/SiteDAL.cs
+++ m2-csharp-capstone/Capstone/DAL/SiteDAL.cs
@@ -10,13 +10,45 @@ namespace Capstone.DAL
 {
     public class SiteDAL
     {
-        /*private string connectionString;
+        private string connectionString;
 
         public SiteDAL(string dBconnectionString)
         {
             connectionString = dBconnectionString;
         }
-        public List<Site> GetAvailableReservation(int campgroundId, DateTime startDate, DateTime endDate)
+        public List<Reservation> GetReservation(int campgroundId)
+        {
+            List<Reservation> output = new List<Reservation>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("Select * from reservation inner join site on site.site_id = reservation.site_id inner join campground on campground.campground_id = site.campground_id where campground.campground_id = @campground_id", conn);
+                    cmd.Parameters.AddWithValue("@campground_id", campgroundId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Reservation r = new Reservation();
+                        r.Id = Convert.ToInt32(reader["reservation_id"]);
+                        r.SiteId = Convert.ToInt32(reader["site_id"]);
+                        r.Name = Convert.ToString(reader["name"]);
+                        r.StartDate = Convert.ToDateTime(reader["from_date"]);
+                        r.EndDate = Convert.ToDateTime(reader["to_date"]);
+                        output.Add(r);
+                    }
+                  
+
+                }
+                
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+            return output;
+        }
+        public List<Site> GetSites(int campgroundId)
         {
             List<Site> output = new List<Site>();
             try
@@ -24,10 +56,46 @@ namespace Capstone.DAL
                 using (SqlConnection conn = new SqlConnection(connectionString))
                 {
                     conn.Open();
+                    SqlCommand cmd = new SqlCommand("Select * from site inner join campground on campground.campground_id = site.campground_id where campground.campground_id = @campground_id", conn);
+                    cmd.Parameters.AddWithValue("@campground_id", campgroundId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Site s = new Site();
+                        s.Id = Convert.ToInt32(reader["site_id"]);
+                        s.CampgroundId = Convert.ToInt32(reader["campground_id"]);
+                        s.SiteNumber = Convert.ToInt32(reader["site_number"]);
+                        s.MaxOccupancy = Convert.ToInt32(reader["max_occupancy"]);
+                        if (Convert.ToInt32(reader["accessible"]) == 1)
+                        {
+                            s.isAccessible = true;
+                        }
+                        else
+                        {
+                            s.isAccessible = false;
+                        }
+                        if (Convert.ToInt32(reader["utilities"]) == 1)
+                        {
+                            s.hasUtilities = true;
+                        }
+                        else
+                        {
+                            s.hasUtilities = false;
+                        }
+                        s.MaxRVLength = Convert.ToInt32(reader["max_rv_length"]);
+                        output.Add(s);
+                    }
+
 
                 }
+
             }
-        }*/
+            catch (SqlException ex)
+            {
+                throw;
+            }
+            return output;
+        }
 
     }
 }
